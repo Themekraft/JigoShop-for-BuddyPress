2efc207662b9f995c11dde5a67e53d0a1026c677
Boris Glumpler <boris@shabushabu.eu>
2011-09-30 20:55:57 +0100

    Added check for manual file downloads when time restriction is in place

--- core/bpshop-downloads.php
+++ core/bpshop-downloads.php
@@ -21,15 +21,18 @@ class BPSHOP_Downloads
 	 */
 	public function init()
 	{
+		remove_action( 'init', 'jigoshop_download_product' );
+		
 		add_action( 'additional_downloadable_product_type_options', array( __CLASS__, 'add_time_option'  ) 	  );
 		add_action( 'save_post', 									array( __CLASS__, 'save_time_option' ), 1 );
+		add_action( 'init', 										array( __CLASS__, 'download_product' )	  );
 	}
 	
 	/**
 	 * Add the time-based option to product pages
 	 * 
 	 * @since 	1.0
-	 * @access 	private
+	 * @access 	public
 	 */
 	public function add_time_option()
 	{
@@ -58,9 +61,9 @@ class BPSHOP_Downloads
 	 * Save the time-based option
 	 * 
 	 * @since 	1.0
-	 * @access 	private
+	 * @access 	public
 	 */
-	function save_time_option( $post_id )
+	public function save_time_option( $post_id )
 	{	
 		if( isset( $_POST['time_limit']['length'] ) && absint( $_POST['time_limit']['length'] ) > 0 && isset( $_POST['time_limit']['duration'] ) && in_array( $_POST['time_limit']['duration'], array( 'days', 'weeks', 'months', 'years' ) ) )
 			update_post_meta( $post_id, 'time_limit', $_POST['time_limit'] );
@@ -72,9 +75,9 @@ class BPSHOP_Downloads
 	 * Probably not needed, but applies to a geeks sense of neatness
 	 * 
 	 * @since 	1.0
-	 * @access 	private
+	 * @access 	public
 	 */
-	function get_duration( $duration = false, $length = false )
+	public function get_duration( $duration = false, $length = false )
 	{
 		if( ! $duration || ! $length )
 			return false;
@@ -109,9 +112,8 @@ class BPSHOP_Downloads
 	 * as it's BuddyPress agnostic
 	 * 
 	 * @since 	1.0
-	 * @access 	private
+	 * @access 	public
 	 * @todo	Pull request for Jigoshop team
-	 * 			Need to check if downloads are manually accessible (should be handled by Jigoshop, really)
 	 */
 	public function get_downloadable_products()
 	{
@@ -173,6 +175,158 @@ class BPSHOP_Downloads
 		endif;			
 
 		return $downloads;
+	}
+
+	/**
+	 * Download a product
+	 * 
+	 * Based on jigoshop_download_product()
+	 * Should really be done via a filter
+	 * 
+	 * @since 	1.0
+	 * @access 	public
+	 * @todo	Pull request for Jigoshop team
+	 */
+	public function download_product()
+	{
+		if( isset( $_GET['download_file'] ) && isset( $_GET['order'] ) && isset( $_GET['email'] ) ) :
+	
+			global $wpdb;
+	
+			$download_file = (int) urldecode( $_GET['download_file'] );
+			$order = urldecode( $_GET['order'] );
+			$email = urldecode( $_GET['email'] );
+	
+			if( ! is_email( $email ) )
+				wp_safe_redirect( home_url() );
+	
+			$downloads_remaining = $wpdb->get_var( $wpdb->prepare( "
+				SELECT downloads_remaining
+				FROM {$wpdb->prefix}jigoshop_downloadable_product_permissions
+				WHERE user_email = %s
+				AND order_key = %s
+				AND product_id = %d
+			", $email, $order, $download_file ) );
+			
+			// Restrict a download once time has passed
+			if( $limit = (array) get_post_meta( $download_file, 'time_limit', true ) ) :
+				
+				// get order_id from order_key
+				$order_id = (int) $wpdb->get_var( $wpdb->prepare( "
+					SELECT post_id
+					FROM {$wpdb->postmeta}
+					WHERE meta_key = 'order_key'
+					AND meta_value = %s
+				", $order ) );
+
+				$order = new jigoshop_order( $order_id );
+
+				$duration = self::get_duration( $limit['duration'], $limit['length'] );							
+				$downloadable_until = strtotime( '+'. $limit['length'] .' '. $duration , strtotime( $order->order_date ) );
+
+				if( $downloadable_until < strtotime( 'now' ) )
+		            wp_die( sprintf( __( 'The time limit for this file has been reached and it cannot be downloaded any more. <a href="%s">Go to homepage &rarr;</a>', 'bpshop' ), home_url() ) );
+			endif;
+
+			// Resume default Jigoshop function
+	        if( $downloads_remaining == NULL ) :
+	            wp_die( sprintf( __( 'File not found. <a href="%s">Go to homepage &rarr;</a>', 'jigoshop' ), home_url() ) );
+			elseif( $downloads_remaining == '0' ) :
+	            wp_die( sprintf( __( 'Sorry, you have reached your download limit for this file. <a href="%s">Go to homepage &rarr;</a>', 'jigoshop' ), home_url() ) );
+			else :
+				if( $downloads_remaining > 0 ) :
+					$wpdb->update( $wpdb->prefix ."jigoshop_downloadable_product_permissions", array(
+						'downloads_remaining' => $downloads_remaining - 1,
+					), array(
+						'user_email' 	=> $email,
+						'order_key'		=> $order,
+						'product_id' 	=> $download_file
+					), array( '%d' ), array( '%s', '%s', '%d' ) );
+				endif;
+	
+				// Download the file
+				$file_path = ABSPATH . get_post_meta( $download_file, 'file_path', true );
+	
+	            $file_path = realpath( $file_path );
+	            
+	            if( ! file_exists( $file_path ) || is_dir( $file_path ) || ! is_readable( $file_path ) )
+	                wp_die( sprintf(__( 'File not found. <a href="%s">Go to homepage &rarr;</a>', 'jigoshop' ), home_url() ) );
+	
+	            $file_extension = strtolower( substr( strrchr( $file_path, "." ), 1 ) );
+	
+	            switch( $file_extension ) :
+	                case "pdf":
+	                	$ctype = "application/pdf";
+	                	break;
+						
+	                case "exe":
+	                	$ctype = "application/octet-stream";
+	                	break;
+						
+	                case "zip":
+	                	$ctype = "application/zip";
+	                	break;
+						
+	                case "doc":
+	                	$ctype = "application/msword";
+	                	break;
+						
+	                case "xls":
+	                	$ctype = "application/vnd.ms-excel";
+	                	break;
+						
+	                case "ppt":
+	                	$ctype = "application/vnd.ms-powerpoint";
+	                	break;
+						
+	                case "gif":
+	                	$ctype = "image/gif";
+	                	break;
+						
+	                case "png":
+	                	$ctype = "image/png";
+	                	break;
+						
+	                case "jpe":
+	                case "jpeg":
+	                case "jpg":
+	                	$ctype = "image/jpg";
+	                	break;
+	                	
+	                default:
+	                	$ctype = "application/force-download";
+	            endswitch;
+	
+				@ini_set( 'zlib.output_compression', 'Off' );
+				@set_time_limit( 0 );
+				@session_start();
+				@session_cache_limiter( 'none' );
+				@set_magic_quotes_runtime( 0 );
+				@ob_end_clean();
+				@session_write_close();
+	
+				header( "Pragma: no-cache" );
+				header( "Expires: 0" );
+				header( "Cache-Control: must-revalidate, post-check=0, pre-check=0" );
+				header( "Robots: none" );
+				header( "Content-Type: ". $ctype ."" );
+				header( "Content-Description: File Transfer" );
+	
+	          	if( strstr( $_SERVER['HTTP_USER_AGENT'], "MSIE" ) )
+	          	{
+				    // workaround for IE filename bug with multiple periods / multiple dots in filename
+				    $iefilename = preg_replace( '/\./', '%2e', basename( $file_path ), substr_count( basename( $file_path ), '.' ) - 1 );
+				    header( "Content-Disposition: attachment; filename=\"". $iefilename ."\";" );
+				} else
+				    header( "Content-Disposition: attachment; filename=\"". basename( $file_path ) ."\";" );
+	
+				header( "Content-Transfer-Encoding: binary" );
+	
+	            header( "Content-Length: ". @filesize( $file_path ) );
+	            @readfile( "$file_path" ) or wp_die( sprintf(__(' File not found. <a href="%s">Go to homepage &rarr;</a>', 'jigoshop' ), home_url() ) );
+				exit;
+			endif;
+		endif;
 	}
 }
 BPSHOP_Downloads::init();
